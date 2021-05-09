fece27cd2c814281af7b014df4767baf8d8bdbef
Mark Harmstone <mark@harmstone.com>
2021-05-09 17:56:35 +0100

    add clang-amd64.cmake

---
+++ clang-amd64.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang)
+set(CMAKE_AR llvm-ar)
+set(CMAKE_RANLIB llvm-ranlib)
+set(CMAKE_SYSTEM_PROCESSOR "AMD64")
+
+set(CLANG_TARGET_TRIPLE x86_64-w64-mingw32)
+set(CMAKE_C_COMPILER_TARGET x86_64-w64-mingw32)
+set(CMAKE_CXX_COMPILER_TARGET x86_64-w64-mingw32)
+set(CMAKE_ASM_COMPILER_TARGET x86_64-w64-mingw32)
+
+set(CMAKE_EXE_LINKER_FLAGS "-static -nodefaultlibs -nostdlib -fuse-ld=lld")
+
+set(CMAKE_C_COMPILER_WORKS 1)
+set(CMAKE_CXX_COMPILER_WORKS 1)
